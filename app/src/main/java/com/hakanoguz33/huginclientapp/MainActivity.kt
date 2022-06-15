84ca7ba8f1915c8eda5b462f355502910ac7402a
Hakan Oğuz <hakanoguz33@yandex.com>
2022-06-15 04:26:07 +0300

    Initial commit

---
+++ app/src/main/java/com/hakanoguz33/huginclientapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hakanoguz33.huginclientapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
