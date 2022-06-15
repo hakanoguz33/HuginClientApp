8e89f36629f799a124662bbca10869e3457d62f0
Hakan Oğuz <hakanoguz33@yandex.com>
2022-06-15 13:11:11 +0300

    .

--- app/src/main/java/com/hakanoguz33/huginclientapp/view/SalesActivity.kt
+++ app/src/main/java/com/hakanoguz33/huginclientapp/view/SalesActivity.kt
@@ -3,12 +3,19 @@ package com.hakanoguz33.huginclientapp.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.hakanoguz33.huginclientapp.MainActivity
 import com.hakanoguz33.huginclientapp.R
 
 class SalesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sales)
+        val btn:Button = findViewById(R.id.btnGoBack)
 
+        btn.setOnClickListener {
+            val intent:Intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
